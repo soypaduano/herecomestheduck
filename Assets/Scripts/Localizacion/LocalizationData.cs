d7de34f57f4fdcf15c81056e726646997edb2ae0
soypaduano <sebastian.paduano.m@gmail.com>
2018-02-21 00:00:32 +0100

    stablish version before getting into the localization

---
+++ Assets/Scripts/Localizacion/LocalizationData.cs
@@ -0,0 +1,13 @@
+﻿public class LocalizationData
+{ 
+
+	
+}
+
+
+[System.Serializable] //Serializamos para poder obtener esto en un JSON
+public class LocalizationItem
+{
+    public string key;
+    public string value;
+}
